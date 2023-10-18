7bba06f6fda081bd3da6871407f70b91c62c5184
Hegewald1 <mads.hegewald@gmail.com>
2023-10-18 08:45:42 +0200

    add custom aliases for zsh shell

---
+++ omz/aliases.zsh
@@ -0,0 +1,15 @@
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+alias zs="source ~/.zshrc"
+alias vpnlogs="journalctl -u NetworkManager.service -fx"
+
+alias fbs="cd ~/Desktop/repos/whitelabels/fbs"
+alias ops="cd ~/Desktop/repos/whitelabels/ops"
+alias wl="cd ~/Desktop/repos/whitelabels/fbs/"
+alias gom=" cd ~/Desktop/repos/new_gomule/gomule/"
