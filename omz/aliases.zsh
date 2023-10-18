332d088023f4a9f905e3282f7b5655ff1caba4a0
Hegewald1 <mads.hegewald@gmail.com>
2023-10-18 09:32:51 +0200

    add alias for cd'ing to personal folder

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -11,5 +11,6 @@ alias vpnlogs="journalctl -u NetworkManager.service -fx"
 
 alias fbs="cd ~/Desktop/repos/whitelabels/fbs"
 alias ops="cd ~/Desktop/repos/whitelabels/ops"
-alias wl="cd ~/Desktop/repos/whitelabels/fbs/"
-alias gom=" cd ~/Desktop/repos/new_gomule/gomule/"
+alias wl="cd ~/Desktop/repos/whitelabels/fbs"
+alias gom="cd ~/Desktop/repos/new_gomule/gomule"
+alias hw1="cd ~/Desktop/repos/Hegewald1"
