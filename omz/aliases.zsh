922de606d4e759f66efce9b0dd9c6d821d32a3fe
Hegewald1 <mads.hegewald@gmail.com>
2023-10-18 09:53:51 +0200

    added alias for nano into aliases

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -1,14 +1,10 @@
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
+# Going to files
 alias zs="nano ~/.zshrc"
+alias zsa="nano ~/Desktop/repos/Hegewald1/omz/aliases.zsh"
+
 alias vpnlogs="journalctl -u NetworkManager.service -fx"
 
+# Folders
 alias fbs="cd ~/Desktop/repos/whitelabels/fbs"
 alias ops="cd ~/Desktop/repos/whitelabels/ops"
 alias wl="cd ~/Desktop/repos/whitelabels/fbs"
