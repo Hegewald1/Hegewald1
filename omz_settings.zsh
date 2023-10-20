b7ed5c34bf569842445bfa4eec6c63666f762503
Hegewald1 <mads.hegewald@gmail.com>
2023-10-20 09:26:14 +0200

    update omz settings to only be single file with all settings

--- omz_settings.zsh
+++ omz_settings.zsh
@@ -1,4 +1,19 @@
-# Going to files
+# asdf path
+. "$HOME/.asdf/asdf.sh"
+
+# Theme:
+ZSH_THEME="af-magic"
+
+# Plugins:
+plugins=(git rails ruby bundler chucknorris history zsh-autosuggestions zsh-syntax-highlighting brew)
+source $ZSH/oh-my-zsh.sh
+
+# Exports:
+export DATABASE_USERNAME=postgres
+export SSH_AUTH_SOCK=~'secret'
+export ZSH="$HOME/.oh-my-zsh"
+
+# Aliases
 alias zs="nano ~/.zshrc"
 alias zsa="nano ~/Desktop/repos/Hegewald1/omz/aliases.zsh"
 
@@ -13,3 +28,4 @@ alias hw1="cd ~/Desktop/repos/Hegewald1"
 
 # Random - Might delete or change later
 alias urp="unicorn_rails -p 3000"
+
