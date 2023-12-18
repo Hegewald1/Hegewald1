4d98404f2cb0f968e51a7bc9083091e969b3bd93
Mads Hegewald <mads.hegewald@gmail.com>
2023-12-18 09:17:16 +0100

    add custom github pill request viewer + checkout function
    credits to Jacob Kjeldahl for this function

--- omz_settings.zsh
+++ omz_settings.zsh
@@ -29,3 +29,15 @@ alias hw1="cd ~/Desktop/repos/Hegewald1"
 # Random - Might delete or change later
 alias urp="unicorn_rails -p 3000"
 
+# Lists the 30 most recently pull requests for the git repository hosted on github
+# Displays a preview with the information abount the PR
+# Enter to checkout the pull request
+# Dependencies: fzf, tail, awk, xargs and gh
+function ghpr() {
+  if [ "$(tput cols)" -gt "120" ]; then
+    window_location=right
+  else
+    window_location=down
+  fi
+  GH_FORCE_TTY=100% gh pr list | tail -n +5 | fzf --ansi --preview 'GH_FORCE_TTY=100% gh pr view {1}' --preview-window $window_location --bind 'ctrl-j:preview-down,ctrl-k:preview-up' | a$
+}
